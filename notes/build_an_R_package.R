320b5ee5b0c158db44752cfe6b826108e9ba6166
Kaifeng Lu <kaifenglu@gmail.com>
2023-02-06 21:21:58 -0500

    update version

--- notes/build_an_R_package.R
+++ notes/build_an_R_package.R
@@ -1,9 +1,6 @@
 Rcpp::compileAttributes()
 devtools::document()
-
-devtools::test()
-
 devtools::build()
 
-system("R CMD check --as-cran ../lrstat_0.1.6.tar.gz")
+system("R CMD check --as-cran ../lrstat_0.1.10.tar.gz")
 
