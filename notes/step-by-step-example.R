13f054b43c3dfb1db829138479dab2ca16702fd9
Kaifeng Lu <kaifenglu@gmail.com>
2023-02-06 22:10:22 -0500

    update call to fseqbon

--- notes/step-by-step-example.R
+++ notes/step-by-step-example.R
@@ -8,13 +8,13 @@ G = matrix(c(0, 1, 0, 0, 0, 0,  0, 0, 0.2, 0.8, 0, 0,
            nrow=6, byrow=TRUE)
 
 alpha = 0.025
-asf = rep("sfOF", 6)
-incid = matrix(c(1, 1, 0, rep(1, 15)), nrow=6, ncol=3, byrow=TRUE)
-maxinfo = c(259, 183, 321, 195, 392, 230)
+typeAlphaSpending = rep("sfOF", 6)
+incidenceMatrix = matrix(c(1, 1, 0, rep(1, 15)), nrow=6, ncol=3, byrow=TRUE)
+maxInformation = c(259, 183, 321, 195, 392, 230)
 
-info = matrix(c(
+information = matrix(c(
   186, 259, 299, 
-  96, 148, 183, 
+   96, 148, 183, 
   225, 296, 324, 
   100, 161, 195, 
   245, 341, 381, 
@@ -31,8 +31,12 @@ p = matrix(c(
   nrow=6, ncol=3, byrow=TRUE)
 
 
-(reject <- fseqbon(w=w, G=G, alpha=alpha, asf=asf, incid=incid, 
-                   maxinfo=maxinfo, info=info, p=p))
+(result <- fseqbon(w = w, G = G, alpha = alpha, kMax = 3,
+                   typeAlphaSpending = typeAlphaSpending, 
+                   incidenceMatrix = incidenceMatrix,
+                   maxInformation = maxInformation, 
+                   p = p,
+                   information = information))
 
 
 # look 1, Itau = {1, 2, 3, 4, 5, 6} 
@@ -40,10 +44,10 @@ I = seq(1,6)
 
 # nominal significance levels for H1
 u = getBound(
-  kMax = 2, informationRates = c(info[1,1]/maxinfo[1], 1), 
-  alpha = w[1]*alpha, typeAlphaSpending = asf[1])
+  k = 1, informationRates = information[1,1]/maxInformation[1], 
+  alpha = w[1]*alpha, typeAlphaSpending = typeAlphaSpending[1])
 
-(astar = 1 - pnorm(u[1]))
+c(p11 = p[1,1], astar = 1 - pnorm(u[1]))
 
 # since p[1,1] < astar, reject H1, update w and G
 (newgraph = updateGraph(w, G, I, 1))
@@ -53,9 +57,9 @@ I = newgraph$I
 
 # nominal significance levels for H2
 u = getBound(
-  kMax = 2, informationRates = c(info[2,1]/maxinfo[2], 1), 
-  alpha = w[2]*alpha, typeAlphaSpending = asf[2])
-(astar = 1 - pnorm(u[1]))
+  k = 1, informationRates = information[2,1]/maxInformation[2], 
+  alpha = w[2]*alpha, typeAlphaSpending = typeAlphaSpending[2])
+c(p21 = p[2,1], astar = 1 - pnorm(u[1]))
 
 # since p[2,1] > astar, cannot reject H2
 # the remaining hypotheses have zero weights and cannot be rejected in look 1
@@ -63,9 +67,9 @@ u = getBound(
 
 # nominal significance level for H2
 u = getBound(
-  kMax = 3, informationRates = c(info[2,1:2]/maxinfo[2], 1), 
-  alpha = w[2]*alpha, typeAlphaSpending = asf[2])
-(astar = 1 - pnorm(u[2]))
+  k = 2, informationRates = information[2,1:2]/maxInformation[2], 
+  alpha = w[2]*alpha, typeAlphaSpending = typeAlphaSpending[2])
+c(p22 = p[2,2], astar = 1 - pnorm(u[2]))
 
 # since p[2,2] < astar, reject H2, update w and G
 (newgraph = updateGraph(w, G, I, 2))
@@ -75,17 +79,17 @@ I = newgraph$I
 
 # nominal significance level for H3
 u = getBound(
-  kMax = 3, informationRates = c(info[3,1:2]/maxinfo[3], 1), 
-  alpha = w[3]*alpha, typeAlphaSpending = asf[3])
-(astar = 1 - pnorm(u[2]))
+  k = 2, informationRates = information[3,1:2]/maxInformation[3], 
+  alpha = w[3]*alpha, typeAlphaSpending = typeAlphaSpending[3])
+c(p32 = p[3,2], astar = 1 - pnorm(u[2]))
 
 # since p[3,2] > astar, cannot reject H3
 
 # nominal significance level for H4
 u = getBound(
-  kMax = 3, informationRates = c(info[4,1:2]/maxinfo[4], 1), 
-  alpha = w[4]*alpha, typeAlphaSpending = asf[4])
-(astar = 1 - pnorm(u[2]))
+  k = 2, informationRates = information[4,1:2]/maxInformation[4], 
+  alpha = w[4]*alpha, typeAlphaSpending = typeAlphaSpending[4])
+c(p42 = p[4,2], astar = 1 - pnorm(u[2]))
 
 # since p[4,2] < astar, reject H4, update w and G
 (newgraph = updateGraph(w, G, I, 4))
@@ -97,9 +101,9 @@ I = newgraph$I
 
 # nominal significance level for H5
 u = getBound(
-  kMax = 3, informationRates = c(info[5,1:2]/maxinfo[5], 1), 
-  alpha = w[5]*alpha, typeAlphaSpending = asf[5])
-(astar = 1 - pnorm(u[2]))
+  k = 2, informationRates = information[5,1:2]/maxInformation[5], 
+  alpha = w[5]*alpha, typeAlphaSpending = typeAlphaSpending[5])
+c(p52 = p[5,2], astar = 1 - pnorm(u[2]))
 
 # since p[5,2] < astar, reject H5, update w and G
 (newgraph = updateGraph(w, G, I, 5))
@@ -111,26 +115,20 @@ I = newgraph$I
 
 # nominal significance level for H6
 u = getBound(
-  kMax = 3, informationRates = c(info[6,1:2]/maxinfo[6], 1), 
-  alpha = w[6]*alpha, typeAlphaSpending = asf[6])
-(astar = 1 - pnorm(u[2]))
+  k = 2, informationRates = information[6,1:2]/maxInformation[6], 
+  alpha = w[6]*alpha, typeAlphaSpending = typeAlphaSpending[6])
+c(p62 = p[6,2], astar = 1 - pnorm(u[2]))
 
 # since p[6,2] > astar, H6 cannot be rejected, go to look 3, Itau = {3, 6}
 
 # nominal significance level for H3, note the observed maximum information
 # is different from the planned maximum information, need to adjust
 # the critical value at the last look
-u1 = getBound(
-  kMax = 3, informationRates = c(info[3,1:2]/maxinfo[3], 1), 
-  alpha = w[3]*alpha, typeAlphaSpending = asf[3])
-
-probs = exitprob(b = u1, a = c(rep(-6,2), u1[3]), theta = 0, I = info[3,])
-
 u = getBound(
-  kMax = 3, informationRates = c(info[3,1:3]/info[3,3]),
-  alpha = w[3]*alpha, typeAlphaSpending = "user",
-  userAlphaSpending = c(cumsum(probs$exitProbUpper)[1:2], w[3]*alpha))
-(astar = 1 - pnorm(u[3]))
+  k = 3, informationRates = information[3,1:3]/information[3,3], 
+  alpha = w[3]*alpha, typeAlphaSpending = typeAlphaSpending[3],
+  spendingTime = c(information[3,1:2]/maxInformation[3], 1))
+c(p33 = p[3,3], astar = 1 - pnorm(u[3]))
 
 # since p[3,3] < astar, H3 is rejected, update w and G
 (newgraph = updateGraph(w, G, I, 3))
@@ -141,17 +139,11 @@ I = newgraph$I
 # nominal significance level for H6, note the observed maximum information
 # is different from the planned maximum information, need to adjust
 # the critical value at the last look
-u1 = getBound(
-  kMax = 3, informationRates = c(info[6,1:2]/maxinfo[6], 1), 
-  alpha = w[6]*alpha, typeAlphaSpending = asf[6])
-
-probs = exitprob(b = u1, a = c(rep(-6,2), u1[3]), theta = 0, I = info[6,])
-
 u = getBound(
-  kMax = 3, informationRates = c(info[6,1:3]/info[6,3]),
-  alpha = w[6]*alpha, typeAlphaSpending = "user",
-  userAlphaSpending = c(cumsum(probs$exitProbUpper)[1:2], w[6]*alpha))
-(astar = 1 - pnorm(u[3]))
+  k = 3, informationRates = information[6,1:3]/information[6,3], 
+  alpha = w[6]*alpha, typeAlphaSpending = typeAlphaSpending[6], 
+  spendingTime = c(information[6,1:2]/maxInformation[6], 1))
+c(p64 = p[6,3], astar = 1 - pnorm(u[3]))
 
 # since p[6,3] > astar, H6 cannot be rejected, the procedure stops
 # the final decision is to reject H1 at look 1, reject H2, H4, and H5 
