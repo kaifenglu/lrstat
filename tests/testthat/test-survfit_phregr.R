0a49330e6986742896b8e6db0b93fbf3020f16b6
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-12 00:14:10 -0400

    only compare time > 0 records and add ncensor variable to the comparison between survfit_phregr and survfit_coxph

--- tests/testthat/test-survfit_phregr.R
+++ tests/testthat/test-survfit_phregr.R
@@ -20,12 +20,14 @@ testthat::test_that("survfit_phregr: right-censored data", {
   surv2 <- survfit(fit2, newdata=temp, conf.type="log-log")
 
   # extract common variables
-  surv1b <- surv1 %>% select(time, nrisk, nevent, cumhaz, surv,
-                             sesurv, lower, upper)
+  surv1b <- surv1 %>%
+    select(time, nrisk, nevent, ncensor, cumhaz,
+           surv, sesurv, lower, upper) %>%
+    slice(-1)
 
   surv2b <- data.frame(time = surv2$time, nrisk = surv2$n.risk,
-                       nevent = surv2$n.event, cumhaz = surv2$cumhaz,
-                       surv = surv2$surv,
+                       nevent = surv2$n.event, ncensor = surv2$n.censor,
+                       cumhaz = surv2$cumhaz, surv = surv2$surv,
                        sesurv = surv2$surv*surv2$std.chaz,
                        lower = surv2$lower, upper = surv2$upper)
 
@@ -56,23 +58,30 @@ testthat::test_that("survfit_phregr: stratified analysis", {
   # of note, surv1 is one data frame ordered by strata and covariates,
   # in contrast, surv2 has the strata in rows and covariates in columns
   # need to rearrange surv2 to have the same layout as surv1
-  surv1b <- surv1 %>% select(time, nrisk, nevent, cumhaz, surv,
-                             sesurv, lower, upper, edema)
+  surv1b <- surv1 %>%
+    mutate(bili = exp(log.bili.)) %>%
+    select(time, nrisk, nevent, cumhaz, surv,
+           sesurv, lower, upper, edema, age, bili) %>%
+    group_by(edema, age, bili) %>%
+    slice(-1) %>%
+    ungroup()
 
-  strata <- rep(as.numeric(substring(attr(surv2$strata, "names"), 7)),
-                surv2$strata)
-  surv2b <- data.frame(time = surv2$time, nrisk = surv2$n.risk,
-                       nevent = surv2$n.event, cumhaz = surv2$cumhaz[,1],
-                       surv = surv2$surv[,1],
-                       sesurv = surv2$surv[,1]*surv2$std.chaz[,1],
-                       lower = surv2$lower[,1], upper = surv2$upper[,1],
-                       edema = strata) %>%
-    bind_rows(data.frame(time = surv2$time, nrisk = surv2$n.risk,
-                         nevent = surv2$n.event, cumhaz = surv2$cumhaz[,2],
-                         surv = surv2$surv[,2],
-                         sesurv = surv2$surv[,2]*surv2$std.chaz[,2],
-                         lower = surv2$lower[,2], upper = surv2$upper[,2],
-                         edema = strata)) %>%
+  strata <- rep(as.numeric(substring(names(surv2$strata), 7)), surv2$strata)
+
+  surv2b <- bind_rows(
+    tibble(time = surv2$time, nrisk = surv2$n.risk,
+           nevent = surv2$n.event, cumhaz = surv2$cumhaz[,1],
+           surv = surv2$surv[,1],
+           sesurv = surv2$surv[,1]*surv2$std.chaz[,1],
+           lower = surv2$lower[,1], upper = surv2$upper[,1],
+           edema = strata) %>% bind_cols(surv2$newdata[1,]),
+    tibble(time = surv2$time, nrisk = surv2$n.risk,
+           nevent = surv2$n.event, cumhaz = surv2$cumhaz[,2],
+           surv = surv2$surv[,2],
+           sesurv = surv2$surv[,2]*surv2$std.chaz[,2],
+           lower = surv2$lower[,2], upper = surv2$upper[,2],
+           edema = strata) %>%
+      bind_cols(surv2$newdata[2,])) %>%
     arrange(edema)
 
   testthat::expect_equal(surv1b, surv2b)
@@ -113,12 +122,14 @@ testthat::test_that("survfit_phregr: time-dependent covariates", {
   surv1 <- survfit_phregr(fit1, newdata=temp)
   surv2 <- survfit(fit2, newdata=temp, id=id, conf.type="log-log")
 
-  surv1b <- surv1 %>% select(time, nrisk, nevent, cumhaz, surv,
-                             sesurv, lower, upper)
+  surv1b <- surv1 %>%
+    select(time, nrisk, nevent, ncensor, cumhaz,
+           surv, sesurv, lower, upper) %>%
+    slice(-1)
 
   surv2b <- data.frame(time = surv2$time, nrisk = surv2$n.risk,
-                       nevent = surv2$n.event, cumhaz = surv2$cumhaz,
-                       surv = surv2$surv,
+                       nevent = surv2$n.event, ncensor = surv2$n.censor,
+                       cumhaz = surv2$cumhaz, surv = surv2$surv,
                        sesurv = surv2$surv*surv2$std.chaz,
                        lower = surv2$lower, upper = surv2$upper)
 
