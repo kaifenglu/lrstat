e19391f2dae6b977a9fc0850e540a045367e013f
Kaifeng Lu <kaifenglu@gmail.com>
2026-02-05 07:13:11 -0500

    use pure c++

--- src/logistic_regression.h
+++ src/logistic_regression.h
@@ -1,27 +1,28 @@
-#include <Rcpp.h>
-#include <R_ext/Applic.h>
-
-using namespace Rcpp;
-
 #ifndef __LOGISTIC_REGRESSION__
 #define __LOGISTIC_REGRESSION__
 
-List logisregcpp(const DataFrame data,
-                 const StringVector& rep,
-                 const std::string event,
-                 const StringVector& covariates,
-                 const std::string freq,
-                 const std::string weight,
-                 const std::string offset,
-                 const std::string id,
-                 const std::string link,
-                 const NumericVector& init,
-                 const bool robust,
-                 const bool firth,
-                 const bool flic,
-                 const bool plci,
-                 const double alpha,
-                 const int maxiter,
-                 const double eps);
+struct DataFrameCpp; 
+struct ListCpp; 
+
+#include <string>
+#include <vector>
+
+ListCpp logisregcpp(const DataFrameCpp& data,
+                    const std::string& event = "event",
+                    const std::vector<std::string>& covariates = {},
+                    const std::string& freq = "",
+                    const std::string& weight = "",
+                    const std::string& offset = "",
+                    const std::string& id = "",
+                    const std::string& link = "logit",
+                    const std::vector<double>& init = {},
+                    const bool robust = false,
+                    const bool firth = false,
+                    const bool flic = false,
+                    const bool plci = false,
+                    const double alpha = 0.05,
+                    const int maxiter = 50,
+                    const double eps = 1.0e-9);
+
 
 #endif // __LOGISTIC_REGRESSION__
