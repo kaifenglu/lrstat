203fc63c14bed41d043334cb432b770483304e1b
Kaifeng Lu <kaifenglu@gmail.com>
2026-04-26 13:54:43 -0400

    new mams_design.h

---
+++ src/mams_design.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <cstddef>       // size_t
+#include <cstdint>       // std::int64_t
+#include <list>          // std::list
+#include <mutex>         // std::mutex
+#include <string>        // std::string
+#include <unordered_map> // std::unordered_map
+#include <vector>        // std::vector
+
+#include "utilities.h"
+
+struct FlatMatrix;
+struct ListCpp;
+
+std::vector<double> exitprob_mams_cpp(
+    const size_t M,
+    const double r,
+    const std::vector<double>& theta,
+    const bool corr_known,
+    const size_t kMax,
+    const FlatMatrix& b,
+    const std::vector<double>& I);
+
+std::vector<double> getBound_mams_cpp(
+    const size_t M,
+    const double r,
+    const bool corr_known,
+    const size_t k,
+    const std::vector<double>& informationRates,
+    const double alpha,
+    const std::string& typeAlphaSpending,
+    const double parameterAlphaSpending,
+    const std::vector<double>& userAlphaSpending,
+    const std::vector<double>& spendingTime,
+    const std::vector<unsigned char>& efficacyStopping);
+
+ListCpp getDesign_mams_cpp(
+    const double beta,
+    const double IMax,
+    const std::vector<double>& theta,
+    const size_t M,
+    const double r,
+    const bool corr_known,
+    const size_t kMax,
+    const std::vector<double>& informationRates,
+    const std::vector<unsigned char>& efficacyStopping,
+    const std::vector<double>& criticalValues,
+    const double alpha,
+    const std::string& typeAlphaSpending,
+    const double parameterAlphaSpending,
+    const std::vector<double>& userAlphaSpending,
+    const std::vector<double>& spendingTime);
+
+ListCpp adaptDesign_mams_cpp(
+    double betaNew,
+    double INew,
+    const size_t M,
+    const double r,
+    const bool corr_known,
+    const size_t L,
+    const std::vector<double>& zL,
+    const std::vector<double>& theta,
+    const double IMax,
+    const size_t kMax,
+    const std::vector<double>& informationRates,
+    const std::vector<unsigned char>& efficacyStopping,
+    const std::vector<double>& criticalValues,
+    const double alpha,
+    const std::string& typeAlphaSpending,
+    const double parameterAlphaSpending,
+    const std::vector<double>& userAlphaSpending,
+    const std::vector<double>& spendingTime,
+    const bool MullerSchafer,
+    const size_t MNew,
+    const std::vector<int>& selected,
+    const double rNew,
+    const size_t kNew,
+    const std::vector<double>& informationRatesNew,
+    const std::vector<unsigned char>& efficacyStoppingNew,
+    const std::string& typeAlphaSpendingNew,
+    const double parameterAlphaSpendingNew,
+    const std::vector<double>& spendingTimeNew);
