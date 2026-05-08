a8d4fa374a76b36db7caaee68f6540c423c1dc00
Kaifeng Lu <kaifenglu@gmail.com>
2026-05-08 19:49:33 -0400

    -update exitprob_mams_cpp -add getPower_mams -update getDesign_mams_cpp and adaptDesign_mams_cpp by adding futility boundary parameters

--- src/mams_design.h
+++ src/mams_design.h
@@ -13,7 +13,17 @@
 struct FlatMatrix;
 struct ListCpp;
 
-std::vector<double> exitprob_mams_cpp(
+ListCpp exitprob_mams_cpp(
+    const size_t M,
+    const double r,
+    const std::vector<double>& theta,
+    const bool corr_known,
+    const size_t kMax,
+    const FlatMatrix& b,
+    const FlatMatrix& a,
+    const std::vector<double>& I);
+
+ListCpp exitprob_mams_cpp(
     const size_t M,
     const double r,
     const std::vector<double>& theta,
@@ -35,6 +45,21 @@ std::vector<double> getBound_mams_cpp(
     const std::vector<double>& spendingTime,
     const std::vector<unsigned char>& efficacyStopping);
 
+ListCpp getPower_mams(
+    const size_t M,
+    const double r,
+    const std::vector<double>& theta,
+    const double alpha,
+    const size_t kMax,
+    const std::vector<double>& critValues,
+    const std::vector<double>& I,
+    const std::string& bsf,
+    const double bsfpar,
+    const std::vector<double>& st,
+    const std::vector<unsigned char>& futStopping,
+    const double IL,
+    const std::vector<double>& zL);
+
 ListCpp getDesign_mams_cpp(
     const double beta,
     const double IMax,
@@ -45,11 +70,18 @@ ListCpp getDesign_mams_cpp(
     const size_t kMax,
     const std::vector<double>& informationRates,
     const std::vector<unsigned char>& efficacyStopping,
-    const std::vector<double>& criticalValues,
+    const std::vector<unsigned char>& futilityStopping,
+    const FlatMatrix& criticalValues,
     const double alpha,
     const std::string& typeAlphaSpending,
     const double parameterAlphaSpending,
     const std::vector<double>& userAlphaSpending,
+    const std::vector<double>& futilityBounds,
+    const std::vector<double>& futilityCP,
+    const std::vector<double>& futilityTheta,
+    const std::string& typeBetaSpending,
+    const double parameterBetaSpending,
+    const std::vector<double>& userBetaSpending,
     const std::vector<double>& spendingTime);
 
 ListCpp adaptDesign_mams_cpp(
@@ -65,11 +97,17 @@ ListCpp adaptDesign_mams_cpp(
     const size_t kMax,
     const std::vector<double>& informationRates,
     const std::vector<unsigned char>& efficacyStopping,
-    const std::vector<double>& criticalValues,
+    const std::vector<unsigned char>& futilityStopping,
+    const FlatMatrix& criticalValues,
     const double alpha,
     const std::string& typeAlphaSpending,
     const double parameterAlphaSpending,
     const std::vector<double>& userAlphaSpending,
+    const std::vector<double>& futilityBounds,
+    const std::vector<double>& futilityCP,
+    const std::vector<double>& futilityTheta,
+    const std::string& typeBetaSpending,
+    const double parameterBetaSpending,
     const std::vector<double>& spendingTime,
     const bool MullerSchafer,
     const size_t MNew,
@@ -78,6 +116,13 @@ ListCpp adaptDesign_mams_cpp(
     const size_t kNew,
     const std::vector<double>& informationRatesNew,
     const std::vector<unsigned char>& efficacyStoppingNew,
+    const std::vector<unsigned char>& futilityStoppingNew,
     const std::string& typeAlphaSpendingNew,
     const double parameterAlphaSpendingNew,
+    const std::vector<double>& futilityBoundsInt,
+    const std::vector<double>& futilityCPInt,
+    const std::vector<double>& futilityThetaInt,
+    const std::string& typeBetaSpendingNew,
+    const double parameterBetaSpendingNew,
+    const std::vector<double>& userBetaSpendingNew,
     const std::vector<double>& spendingTimeNew);
